a1fd38366849c91b4a1d4f9ddd0ec1d42e4ddcf0
trayoub <traikiayoub@gmail.com>
2022-11-12 15:52:23 +0100

    feature : almost done

--- gateway/src/main/java/org/sid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -16,7 +16,7 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
- 
+
 
     @Bean
     DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
